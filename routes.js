3ac5055826a7f7cf0d5c636647c788b3c63137bb
Paddym1991 <paddym1991.pm@gmail.com>
2017-09-19 12:16:54 +0100

    public folder created for images and images/assets accessible

--- routes.js
+++ routes.js
@@ -1,7 +1,15 @@
 const Donations = require('./app/controllers/donations');
+const Assets = require('./app/controllers/assets');
 
 module.exports = [
 
   { method: 'GET', path: '/', config: Donations.home },
+
+  {
+    method: 'GET',
+    path: '/{param*}',
+    config: { auth: false },
+    handler: Assets.servePublicDirectory,
+  },
 
 ];
