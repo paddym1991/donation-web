be3b56d4bc813647109ba1e1391faa308097cdfe
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-10 10:50:25 +0100

    Moved data.json(preloaded data file) file.

--- app/models/db.js
+++ app/models/db.js
@@ -20,7 +20,7 @@ mongoose.connection.on('connected', function () {
   //preloading test data, using seeder utility
   if (process.env.NODE_ENV != 'production') {
     var seeder = require('mongoose-seeder');
-    const data = require('./data.json');
+    const data = require('../../data.json');
     const Donation = require('./donation');
     const User = require('./user');
     seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
