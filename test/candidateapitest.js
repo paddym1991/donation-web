923c9f02bd0e69caeeed35423abb16f61bfc2b34
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-17 12:34:31 +0100

    First Mocha API Test.

--- test/candidateapitest.js
+++ test/candidateapitest.js
@@ -1,16 +1,46 @@
 'use strict';
 
 const assert = require('chai').assert;
+var request = require('sync-request');
 
 /**
- * This is a simple test, guaranteed to succeed
+ * Revised vesion of first test to include sync-request
  */
 suite('Candidate API tests', function () {
 
   test('get candidates', function () {
 
-    assert.equal(1, 1);   //try 'assert.equal(1, 2)' to guarantee a failed test
+    const url = 'http://localhost:4000/api/candidates';
+    const res = request('GET', url);
+    const candidates = JSON.parse(res.getBody('utf8'));
+
+    assert.equal(2, candidates.length);
+
+    assert.equal(candidates[0].firstName, 'Lisa');
+    assert.equal(candidates[0].lastName, 'Simpson');
+    assert.equal(candidates[0].office, 'President');
+
+    assert.equal(candidates[1].firstName, 'Donald');
+    assert.equal(candidates[1].lastName, 'Simpson');
+    assert.equal(candidates[1].office, 'President');
 
   });
+
+  test('get one candidate', function () {
+
+    const allCandidatesUrl = 'http://localhost:4000/api/candidates';
+    var res = request('GET', allCandidatesUrl);
+    const candidates = JSON.parse(res.getBody('utf8'));
+
+    const oneCandidateUrl = allCandidatesUrl + '/' + candidates[0]._id;
+    res = request('GET', oneCandidateUrl);
+    const oneCandidate = JSON.parse(res.getBody('utf8'));
+
+    assert.equal(oneCandidate.firstName, 'Lisa');
+    assert.equal(oneCandidate.lastName, 'Simpson');
+    assert.equal(oneCandidate.office, 'President');
+
+  });
+
 });
 
