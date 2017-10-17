16a1b94debe65570bbaaf9ee88ea2573741a3f16
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-17 12:14:42 +0100

    First simple test.

---
+++ test/candidateapitest.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('chai').assert;
+
+/**
+ * This is a simple test, guaranteed to succeed
+ */
+suite('Candidate API tests', function () {
+
+  test('get candidates', function () {
+
+    assert.equal(1, 1);   //try 'assert.equal(1, 2)' to guarantee a failed test
+
+  });
+});
+
