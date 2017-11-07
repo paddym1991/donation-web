8f4ea6d1a1d94303bd37732c5b28fa3ea80b2500
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-07 12:04:33 +0000

    Test fixture & simple test.

--- test/candidateapitest.js
+++ test/candidateapitest.js
@@ -1,87 +1,21 @@
 'use strict';
 
 const assert = require('chai').assert;
-var request = require('sync-request');
+const DonationService = require('./donation-service');
+const fixtures = require('./fixtures.json');
 
-/**
- * Revised vesion of first test to include sync-request
- */
 suite('Candidate API tests', function () {
 
-  test('get candidates', function () {
+  let candidates = fixtures.candidates;
+  let newCandidate = fixtures.newCandidate;
 
-    const url = 'http://localhost:4000/api/candidates';
-    const res = request('GET', url);
-    const candidates = JSON.parse(res.getBody('utf8'));
-
-    assert.equal(2, candidates.length);
-
-    assert.equal(candidates[0].firstName, 'Lisa');
-    assert.equal(candidates[0].lastName, 'Simpson');
-    assert.equal(candidates[0].office, 'President');
-
-    assert.equal(candidates[1].firstName, 'Donald');
-    assert.equal(candidates[1].lastName, 'Simpson');
-    assert.equal(candidates[1].office, 'President');
-
-  });
-
-  test('get one candidate', function () {
-
-    const allCandidatesUrl = 'http://localhost:4000/api/candidates';
-    let res = request('GET', allCandidatesUrl);
-    const candidates = JSON.parse(res.getBody('utf8'));
-
-    const oneCandidateUrl = allCandidatesUrl + '/' + candidates[0]._id;
-    res = request('GET', oneCandidateUrl);
-    const oneCandidate = JSON.parse(res.getBody('utf8'));
-
-    assert.equal(oneCandidate.firstName, 'Lisa');
-    assert.equal(oneCandidate.lastName, 'Simpson');
-    assert.equal(oneCandidate.office, 'President');
-
-  });
+  const donationService = new DonationService('http://localhost:4000');
 
   test('create a candidate', function () {
-
-    const candidatesUrl = 'http://localhost:4000/api/candidates';
-    //invoke a POST route using the sync-request library
-    const newCandidate = {
-      firstName: 'Barnie',
-      lastName: 'Grumble',
-      office: 'President',
-    };
-
-    const res = request('POST', candidatesUrl, { json: newCandidate }); //end of POST
-    const returnedCandidate = JSON.parse(res.getBody('utf8'));
-
-    assert.equal(returnedCandidate.firstName, 'Barnie');
-    assert.equal(returnedCandidate.lastName, 'Grumble');
-    assert.equal(returnedCandidate.office, 'President');
-
+    const returnedCandidate = donationService.createCandidate(newCandidate);
+    assert.equal(returnedCandidate.firstName, newCandidate.firstName);
+    assert.equal(returnedCandidate.lastName, newCandidate.lastName);
+    assert.equal(returnedCandidate.office, newCandidate.office);
+    assert.isDefined(returnedCandidate._id);
   });
-
-  test('delete one candidate', function () {
-
-    const allCandidatesUrl = 'http://localhost:4000/api/candidates';
-    let res = request('GET', allCandidatesUrl);
-    const candidates = JSON.parse(res.getBody('utf8'));
-
-    const deleteOneCandidateUrl = allCandidatesUrl + '/' + candidates[0]._id;
-    let rem = request('Delete', deleteOneCandidateUrl);
-
-    assert.equal(rem.statusCode, '204');
-
-  });
-
-  test('delete all candidates', function () {
-
-    const allCandidatesUrl = 'http://localhost:4000/api/candidates';
-    let res = request('DELETE', allCandidatesUrl);
-
-    assert.equal(res.statusCode, '204');
-
-  });
-
 });
-
