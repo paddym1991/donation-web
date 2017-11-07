522b6145e683a36cded5652337c6167fa389a773
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-07 12:39:45 +0000

    candidateapi Comprehensive tests.

--- test/candidateapitest.js
+++ test/candidateapitest.js
@@ -13,11 +13,34 @@ suite('Candidate API tests', function () {
 
   const donationService = new DonationService('http://localhost:4000');
 
+  //These (beforeEach & afterEach) are run before and after each test - clearing our the candidates
+  // model so that each test can be considered completely independently
+  beforeEach(function () {
+    donationService.deleteAllCandidates();
+  });
+
+  afterEach(function () {
+    donationService.deleteAllCandidates();
+  });
+
   //simplified test with lodash
   test('create a candidate', function () {
     const returnedCandidate = donationService.createCandidate(newCandidate);
-    assert(_.some([returnedCandidate], newCandidate),  'returnedCandidate must be a superset of newCandidate');
+    assert(_.some([returnedCandidate], newCandidate), 'returnedCandidate must be a superset of newCandidate');
     assert.isDefined(returnedCandidate._id);
+  });
+
+  test('get candidate', function () {
+    const c1 = donationService.createCandidate(newCandidate);
+    const c2 = donationService.getCandidate(c1._id);
+    assert.deepEqual(c1, c2);
+  });
+
+  test('get invalid candidate', function () {
+    const c1 = donationService.getCandidate('1234');
+    assert.isNull(c1);
+    const c2 = donationService.getCandidate('012345678901234567890123');
+    assert.isNull(c2);
   });
 
   test('delete a candidate', function () {
@@ -25,5 +48,30 @@ suite('Candidate API tests', function () {
     assert(donationService.getCandidate(c._id) != null);
     donationService.deleteOneCandidate(c._id);
     assert(donationService.getCandidate(c._id) == null);
+  });
+
+  test('get all candidates', function () {
+    for (let c of candidates) {
+      donationService.createCandidate(c);
+    }
+
+    const allCandidates = donationService.getCandidates();
+    assert.equal(allCandidates.length, candidates.length);
+  });
+
+  test('get candidates detail', function () {
+    for (let c of candidates) {
+      donationService.createCandidate(c);
+    }
+
+    const allCandidates = donationService.getCandidates();
+    for (let i = 0; i < candidates.length; i++) {
+      assert(_.some([allCandidates[i]], candidates[i]), 'returnedCandidate must be a superset of newCandidate');
+    }
+  });
+
+  test('get all candidates empty', function () {
+    const allCandidates = donationService.getCandidates();
+    assert.equal(allCandidates.length, 0);
   });
 });
