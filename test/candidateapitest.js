1904945a3de19b22988e8867132ea36142f2aa9a
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-07 12:16:20 +0000

    Equals superset test using lodash.

--- test/candidateapitest.js
+++ test/candidateapitest.js
@@ -3,6 +3,8 @@
 const assert = require('chai').assert;
 const DonationService = require('./donation-service');
 const fixtures = require('./fixtures.json');
+//We need a comparison that will test to see if the returnedCandidate is a superset of the newCandidate object
+const _ = require('lodash');
 
 suite('Candidate API tests', function () {
 
@@ -11,11 +13,10 @@ suite('Candidate API tests', function () {
 
   const donationService = new DonationService('http://localhost:4000');
 
-  //simplified test will fail. The returned object contains the fields as expected,
-  // but also additional, legitimate, fields that cause the equals test to fail
+  //simplified test with lodash
   test('create a candidate', function () {
     const returnedCandidate = donationService.createCandidate(newCandidate);
-    assert.equal(returnedCandidate, newCandidate);
+    assert(_.some([returnedCandidate], newCandidate),  'returnedCandidate must be a superset of newCandidate');
     assert.isDefined(returnedCandidate._id);
   });
 });
