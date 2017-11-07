3cb03d3e32db497513c6f38f5ce08ed7373f70e4
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-07 10:53:32 +0000

    Added usersapi.

---
+++ app/api/usersapi.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const User = require('../models/user');
+const Boom = require('boom');
+
+exports.find = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    User.find({}).exec().then(users => {
+      reply(users);
+    }).catch(err => {
+      reply(Boom.badImplementation('error accessing db'));
+    });
+  },
+
+};
+
+exports.findOne = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    User.findOne({ _id: request.params.id }).then(user => {
+      reply(user);
+    }).catch(err => {
+      reply(Boom.notFound('id not found'));
+    });
+  },
+
+};
+
+exports.create = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    const user = new User(request.payload);
+    user.save().then(newUser => {
+      reply(newUser).code(201);
+    }).catch(err => {
+      reply(Boom.badImplementation('error creating User'));
+    });
+  },
+
+};
+
+exports.deleteAll = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    User.remove({}).then(err => {
+      reply().code(204);
+    }).catch(err => {
+      reply(Boom.badImplementation('error removing Users'));
+    });
+  },
+
+};
+
+exports.deleteOne = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    User.remove({ _id: request.params.id }).then(user => {
+      reply(User).code(204);
+    }).catch(err => {
+      reply(Boom.notFound('id not found'));
+    });
+  },
+
+};
--- routesapi.js
+++ routesapi.js
@@ -3,8 +3,13 @@ const CandidatesApi = require('./app/api/candidatesapi');
 module.exports = [
   { method: 'GET', path: '/api/candidates', config: CandidatesApi.find },   //route for retrieving all candidates
   { method: 'GET', path: '/api/candidates/{id}', config: CandidatesApi.findOne },   //route for retrieving a single candidate (id must be provided)
-
   { method: 'POST', path: '/api/candidates', config: CandidatesApi.create },    //route used to create a single candidate.
   { method: 'DELETE', path: '/api/candidates/{id}', config: CandidatesApi.deleteOne },    //route used to delete a single candidate (id must be provided)
   { method: 'DELETE', path: '/api/candidates', config: CandidatesApi.deleteAll },   //route used to delete all candidates
+
+  { method: 'GET', path: '/api/users', config: UsersApi.find },
+  { method: 'GET', path: '/api/users/{id}', config: UsersApi.findOne },
+  { method: 'POST', path: '/api/users', config: UsersApi.create },
+  { method: 'DELETE', path: '/api/users/{id}', config: UsersApi.deleteOne },
+  { method: 'DELETE', path: '/api/users', config: UsersApi.deleteAll },
 ];
