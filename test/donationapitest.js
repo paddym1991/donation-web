b566b9ae9790e306caedac63a9ce4628f8c0582c
Paddym1991 <paddym1991.pm@gmail.com>
2017-11-14 10:28:35 +0000

    Donations API Tests.

--- test/donationapitest.js
+++ test/donationapitest.js
@@ -13,14 +13,46 @@ suite('Donation API tests', function () {
   const donationService = new DonationService(fixtures.donationService);
 
   beforeEach(function () {
+    donationService.deleteAllCandidates();
+    donationService.deleteAllDonations();
   });
 
   afterEach(function () {
-
+    donationService.deleteAllCandidates();
+    donationService.deleteAllDonations();
   });
 
-  test('a test function', function () {
-
+  test('create a donation', function () {
+    const returnedCandidate = donationService.createCandidate(newCandidate);
+    donationService.makeDonation(returnedCandidate._id, donations[0]);
+    const returnedDonations = donationService.getDonations(returnedCandidate._id);
+    assert.equal(returnedDonations.length, 1);
+    assert(_.some([returnedDonations[0]], donations[0]), 'returned donation must be a superset of donation');
   });
 
+  test('create multiple donations', function () {
+    const returnedCandidate = donationService.createCandidate(newCandidate);
+    for (let i = 0; i < donations.length; i++) {
+      donationService.makeDonation(returnedCandidate._id, donations[i]);
+    }
+
+    const returnedDonations = donationService.getDonations(returnedCandidate._id);
+    assert.equal(returnedDonations.length, donations.length);
+    for (let i = 0; i < donations.length; i++) {
+      assert(_.some([returnedDonations[i]], donations[i]), 'returned donation must be a superset of donation');
+    }
+  });
+
+  test('delete all donations', function () {
+    const returnedCandidate = donationService.createCandidate(newCandidate);
+    for (let i = 0; i < donations.length; i++) {
+      donationService.makeDonation(returnedCandidate._id, donations[i]);
+    }
+
+    const d1 = donationService.getDonations(returnedCandidate._id);
+    assert.equal(d1.length, donations.length);
+    donationService.deleteAllDonations();
+    const d2 = donationService.getDonations(returnedCandidate._id);
+    assert.equal(d2.length, 0);
+  });
 });
