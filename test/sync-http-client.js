c78997eeab2f2562d9918ac1d84e06e44c42229f
Paddym1991 <paddym1991.pm@gmail.com>
2017-12-04 13:48:19 +0000

    JWT test support classes.

--- test/sync-http-client.js
+++ test/sync-http-client.js
@@ -1,22 +1,47 @@
-//The class assumes we are always dealing with JSON payloads.
-//This is the first example of an ES6 Class in our labs so far. We will be making more use of these modern features in forthcoming labs.
-
 /**
- * This class will encapsulate all lower level http request composition and transmission.
+ * This is a reimplementation of our lowest level htpp encapsulation clas
  * @type {doRequest}
  */
-
 let request = require('sync-request');
 
 class SyncHttpService {
 
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
+    this.authHeadder = null;
+  }
+
+  /**
+   * setAuth requests a token from the server, and stores in in an authHeader property.
+   * get, post and delete then include this header as part of all requests
+   * @param url
+   * @param user
+   * @returns {boolean}
+   */
+  setAuth(url, user) {
+    const res = request('POST', this.baseUrl + url, { json: user });
+    if (res.statusCode == 201) {
+      let payload = JSON.parse(res.getBody('utf8'));
+      if (payload.success) {
+        this.authHeadder = { Authorization: 'bearer ' + payload.token, };
+        return true;
+      }
+    }
+
+    this.authHeadder = null;
+    return false;
+  }
+
+  /**
+   * clearAuth clears out the authHeader token
+   */
+  clearAuth() {
+    this.authHeadder = null;
   }
 
   get(url) {
     let returnedObj = null;
-    let res = request('GET', this.baseUrl + url);
+    let res = request('GET', this.baseUrl + url, { headers: this.authHeadder });
     if (res.statusCode < 300) {
       returnedObj = JSON.parse(res.getBody('utf8'));
     }
@@ -26,7 +51,7 @@ class SyncHttpService {
 
   post(url, obj) {
     let returnedObj = null;
-    let res = request('POST', this.baseUrl + url, { json: obj });
+    let res = request('POST', this.baseUrl + url, { json: obj, headers: this.authHeadder });
     if (res.statusCode < 300) {
       returnedObj = JSON.parse(res.getBody('utf8'));
     }
@@ -34,8 +59,8 @@ class SyncHttpService {
     return returnedObj;
   }
 
-  delete (url) {
-    let res = request('DELETE', this.baseUrl + url);
+  delete(url) {
+    let res = request('DELETE', this.baseUrl + url, { headers: this.authHeadder });
     return res.statusCode;
   }
 }
